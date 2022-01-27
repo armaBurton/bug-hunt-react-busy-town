347fbc6fa3e2e2d9bfc2413a92c6edc048cf97db
armaBurton <armanarma@armaburton.com>
2022-01-27 15:54:51 -0800

    partially finished

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function TrafficLight(props) {
   return (
-    <div className=`traffic-light`>
+    <div className={`.traffic-light`}>
       <div className={`red circle ${props.color = 'red' && 'on'}`}></div>
       <div className={`yellow circle ${props.color = 'yellow' && 'on'}`}></div>
       <div className={`green circle ${props.color = 'green' && 'on'}`}></div>
-    </}
+    </div>);
+}
